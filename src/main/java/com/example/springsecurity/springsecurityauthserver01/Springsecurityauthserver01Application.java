580048290b7af6ec43963c645a26b6aaa11c21e1
Dilun-Panduka <dilunp4@gmail.com>
2020-07-22 10:59:58 +0530

    initial commit

---
+++ src/main/java/com/example/springsecurity/springsecurityauthserver01/Springsecurityauthserver01Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.springsecurityauthserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityauthserver01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurityauthserver01Application.class, args);
+    }
+
+}
