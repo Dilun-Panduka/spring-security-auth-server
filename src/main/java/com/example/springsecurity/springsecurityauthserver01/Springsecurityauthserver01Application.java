b3d25ec2d04b6ef444cb32aff40ae05f0c98dbf1
Dilun-Panduka <dilunp4@gmail.com>
2020-07-24 10:11:40 +0530

    Bug fixed

--- src/main/java/com/example/springsecurity/springsecurityauthserver01/Springsecurityauthserver01Application.java
+++ src/main/java/com/example/springsecurity/springsecurityauthserver01/Springsecurityauthserver01Application.java
@@ -2,10 +2,8 @@ package com.example.springsecurity.springsecurityauthserver01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.springsecurity.springsecurityauthserver01.config"})
 public class Springsecurityauthserver01Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springsecurity/springsecurityauthserver01/controllers/HelloController.java
+++ src/main/java/com/example/springsecurity/springsecurityauthserver01/controllers/HelloController.java
@@ -1,15 +1,14 @@
 package com.example.springsecurity.springsecurityauthserver01.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/helo")
 public class HelloController {
+
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello!";
+        return "hello.html";
     }
 }
